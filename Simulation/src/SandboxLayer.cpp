538b5fd61ec53f199dce1f4e24325390c172b8e8
Arun Alex <arunalex62@gmail.com>
2024-12-06 19:39:55 -0800

    Removed mouse clicking logic for adding particles.

--- Simulation/src/SandboxLayer.cpp
+++ Simulation/src/SandboxLayer.cpp
@@ -20,12 +20,9 @@ void SandboxLayer::OnAttach()
 	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 
 	// Init here
-	m_Particle.ColorBegin = { 254 / 255.0f, 212 / 255.0f, 123 / 255.0f, 1.0f };
-	m_Particle.ColorEnd = { 254 / 255.0f, 109 / 255.0f, 41 / 255.0f, 1.0f };
-	m_Particle.SizeBegin = 0.5f, m_Particle.SizeVariation = 0.3f, m_Particle.SizeEnd = 0.0f;
-	m_Particle.LifeTime = 1.0f;
+	m_Particle.Colour = { 13 / 255.0f, 38 / 255.0f, 212 / 255.0f, 1.0f };
 	m_Particle.Velocity = { 0.0f, 0.0f };
-	m_Particle.VelocityVariation = { 3.0f, 1.0f };
+	m_Particle.Forces = { 0.0f, 0.0f };
 	m_Particle.Position = { 0.0f, 0.0f };
 }
 
@@ -51,25 +48,8 @@ void SandboxLayer::OnUpdate(Timestep ts)
 	m_CameraController.OnUpdate(ts);
 
 	// Render here
-
-	//glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
 	glClearColor(0,0,0, 1.0f);
 	glClear(GL_COLOR_BUFFER_BIT);
-
-	if (GLCore::Input::IsMouseButtonPressed(HZ_MOUSE_BUTTON_LEFT))
-	{
-		auto [x, y] = Input::GetMousePosition();
-		auto width = GLCore::Application::Get().GetWindow().GetWidth();
-		auto height = GLCore::Application::Get().GetWindow().GetHeight();
-
-		auto bounds = m_CameraController.GetBounds();
-		auto pos = m_CameraController.GetCamera().GetPosition();
-		x = (x / width) * bounds.GetWidth() - bounds.GetWidth() * 0.5f;
-		y = bounds.GetHeight() * 0.5f - (y / height) * bounds.GetHeight();
-		m_Particle.Position = { x + pos.x, y + pos.y };
-		for (int i = 0; i < 5; i++)
-			m_ParticleSystem.Emit(m_Particle);
-	}
 
 	m_ParticleSystem.OnUpdate(ts);
 	m_ParticleSystem.OnRender(m_CameraController.GetCamera());
@@ -80,8 +60,6 @@ void SandboxLayer::OnImGuiRender()
 	// ImGui here
 
 	ImGui::Begin("Settings");
-	ImGui::ColorEdit4("Birth Color", glm::value_ptr(m_Particle.ColorBegin));
-	ImGui::ColorEdit4("Death Color", glm::value_ptr(m_Particle.ColorEnd));
-	ImGui::DragFloat("Life Time", &m_Particle.LifeTime, 0.1f, 0.0f, 1000.0f);
+	ImGui::ColorEdit4("Colour", glm::value_ptr(m_Particle.Colour));
 	ImGui::End();
 }
