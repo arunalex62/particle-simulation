c055bfa30aad8af53b1c0fa892965f634622a2e7
Arun Alex <arunalex62@gmail.com>
2024-12-13 15:42:59 -0800

    Added/changed fields to allow real-time parameter tuning.

--- Simulation/src/ParticleSystem.h
+++ Simulation/src/ParticleSystem.h
@@ -24,20 +24,21 @@ public:
 	void OnUpdate(GLCore::Timestep ts);
 	void OnRender(GLCore::Utils::OrthographicCamera& camera);
 	void CheckCollisions(const int particleIndex);
-	float SmoothingKernel(float radius, float distance);
-	float CalculateDensity(const glm::vec2 point);
 	float CalculateBuoyancyForce(const int x, const int y);
 	std::array<std::array<float, 100>, 100> CalculateVorticity(const std::array<std::array<glm::vec2, 100>, 100>& velocityField);
-	std::array<std::array<glm::vec2, 100>, 100> ComputeVorticityGradient(
+	std::array<std::array<glm::vec2, 100>, 100> ComputeNormalizedVorticityGradient(
 		const std::array<std::array<float, 100>, 100>& vorticityField);
 	void ApplyVorticityConfinement(
 		std::array<std::array<glm::vec2, 100>, 100>& velocityField,
 		const std::array<std::array<float, 100>, 100>& vorticityField,
 		const std::array<std::array<glm::vec2, 100>, 100>& vorticityGradient,
-		float epsilon, float deltaTime);
-	void UpdateWaterVaporField(const std::array<std::array<float, 100>, 100>& temperatureField,
+		float deltaTime);
+	void UpdateWaterVaporField(std::array<std::array<float, 100>, 100>& temperatureField,
 		const std::array<std::array<float, 100>, 100>& pressureField, std::array<std::array<float, 100>, 100> vaporField,
 		std::array<std::array<float, 100>, 100> cloudWaterField);
+	std::array<std::array<float, 100>, 100> ComputeDivergence(
+		const std::array<std::array<glm::vec2, 100>, 100>& velocityField);
+	void SetBoundaryConditions();
 	void AdvectVelocityField(
 		const std::array<std::array<glm::vec2, 100>, 100>& oldField,
 		std::array<std::array<glm::vec2, 100>, 100>& newField, float timeStep);
@@ -45,7 +46,10 @@ public:
 		const std::array<std::array<glm::vec2, 100>, 100>& velocityField,
 		std::array<std::array<float, 100>, 100>& newField,
 		float deltaTime);
-
+	float vorticityEpsilon;
+	float buoyancyEpsilon;
+	float gravity;
+	std::string frames_per_second;
 private:
 	std::vector<Particle> m_ParticlePool;
 	std::array<std::array<glm::vec2, 100>, 100> m_VelocityField;
@@ -53,7 +57,6 @@ private:
 	std::array<std::array<float, 100>, 100> m_VaporField;
 	std::array<std::array<float, 100>, 100> m_CloudWaterField;
 	std::array<std::array<float, 100>, 100> m_PressureField;
-	float gravity;
 	GLuint m_QuadVA = 0;
 	std::unique_ptr<GLCore::Utils::Shader> m_ParticleShader;
 	GLint m_ParticleShaderViewProj, m_ParticleShaderTransform, m_ParticleShaderColor;
